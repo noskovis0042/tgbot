a2e1d6a267901e1a3e9405917a6f695da6874449
noskovis0042 <vanishgold1221@gmail.com>
2026-08-05 23:18:59 +0300

    подключение бд

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+import asyncio
+import asyncpg
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
 from aiogram.types import Message
@@ -7,8 +9,25 @@ from aiogram.types import Message
 TOKEN = "token"
 
 bot = Bot(TOKEN)
-
 dp = Dispatcher()
+
+
+# ----------------------------------------------------------
+# БД
+# ----------------------------------------------------------
+
+async def connect_db():
+    global db
+
+    db = await asyncpg.connect(
+        user="postgres",
+        password="123",
+        database="telegram_bot",
+        host="localhost",
+        port=5432
+    )
+
+    print("База данных подключена!")
 
 
 # ----------------------------------------------------------
@@ -18,3 +37,16 @@ dp = Dispatcher()
 @dp.message(CommandStart())
 async def start(message: Message):
     await message.answer("todo")
+
+
+# ----------------------------------------------------------
+# Запуск бота
+# ----------------------------------------------------------
+
+async def main():
+    print("Бот запущен!")
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
